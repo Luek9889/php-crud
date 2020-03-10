f1a12e1faf0efcdff80688c512040e1fc9c54fb8
Luke Pearson <lpearson345610@gmail.com>
2020-03-10 02:32:33 +0000

    Pictures changed to fit relevant project

--- php-crud/index.php
+++ php-crud/index.php
@@ -40,7 +40,7 @@ $statement3->closeCursor();
 <!-- the head section -->
 <head>
 <title>PHP CRUD</title>
-<link rel="stylesheet" type="text/css" href="main.css">
+<link rel="stylesheet" type="text/css" href="sass/main.css">
 <link rel="stylesheet" type="cdn" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <!-- the body section -->
